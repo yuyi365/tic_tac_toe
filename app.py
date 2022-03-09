a102e0b87e189fd6318a582b608e15e7fc706d59
Yuyi <yuyi365@gmail.com>
2022-03-09 12:19:57 -0500

    added FastAPI post endpoint

--- app.py
+++ app.py
@@ -6,16 +6,43 @@ app = FastAPI()
 
 
 class Board(BaseModel):
-    slots: List[int]
-    rows: int
+    row1: List[int]
+    row2: List[int]
+    row3: List[int]
+
+    def place_slot(self, slot: int, token="X"):
+        if slot in self.row1:
+            self.row1[slot] = token
+            return BOARD
+        elif slot in self.row2:
+            self.row2[slot] = token
+            return BOARD
+        elif slot in self.row3:
+            self.row3[slot] = token
+            return BOARD
+        else:
+            return BOARD
+
+
+class Move(BaseModel):
+    slot: int
+    token: str
 
 
 def make_empty_board():
-    board = Board(slots=[1, 2, 3, 4, 5, 6, 7, 8, 9], rows=3)
+    board = Board(row1=[1, 2, 3], row2=[4, 5, 6], row3=[7, 8, 9])
     return board
+
+
+BOARD = make_empty_board()
 
 
 @app.get("/board", response_model=Board)
 async def board():
-    board = make_empty_board()
-    return board
+    return BOARD
+
+
+@app.post("/move", response_model=Move)
+async def create_move(move: Move):
+    BOARD.place_slot(move)
+    return move
--- app_test.py
+++ app_test.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from app import make_empty_board, app
+from app import BOARD, make_empty_board, app
 
 client = TestClient(app)
 
@@ -13,9 +13,35 @@ def test_valid_board_endpoint():
     assert response.status_code == expected_status_code
 
 
-def test_empty_board_contents():
+def test_empty_board_content():
     expected_content = make_empty_board()
 
     response = client.get("/board")
+
+    assert response.json() == expected_content.dict()
+
+
+def test_valid_move_endpoint():
+
+    expected_status = 200
+
+    response = client.post(
+        "/move",
+        headers={"X-Token": "playermove"},
+        json={"slot": 1, "token": "X"},
+    )
+
+    assert response.status_code == expected_status
+
+
+def test_post_response_content():
+
+    expected_content = BOARD
+
+    response = client.post(
+        "/move",
+        headers={"X-Token": "playermove"},
+        json={"slot": 1, "token": "X"},
+    )
 
     assert response.json() == expected_content.dict()
