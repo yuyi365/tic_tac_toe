5b16545c302cd04af6ea0ff75f7373b0bf5673b8
Pierce Edmiston <pierce@8thlight.com>
2022-03-10 14:39:44 -0600

    add type checking with mypy

--- app.py
+++ app.py
@@ -12,13 +12,10 @@ EMPTY_TOKEN = "-"
 class Board(BaseModel):
     slots: List[str]
 
-    def check_avail(self, slot_index: int):
-        if self.slots[slot_index] == EMPTY_TOKEN:
-            return True
-        else:
-            return False
+    def check_avail(self, slot_index: int) -> bool:
+        return self.slots[slot_index] == EMPTY_TOKEN
 
-    def place_slot(self, slot_index: int):
+    def place_slot(self, slot_index: int) -> None:
         if self.check_avail(slot_index):
             self.slots[slot_index] = PLAYER_ONE_TOKEN
 
@@ -27,7 +24,7 @@ class Move(BaseModel):
     slot_index: int
 
 
-def make_empty_board():
+def make_empty_board() -> Board:
     board = Board(slots=["-", "-", "-", "-", "-", "-", "-", "-", "-"])
     return board
 
@@ -36,17 +33,17 @@ state = {}
 
 
 @app.on_event("startup")
-async def startup_event():
+async def startup_event() -> None:
     state["board"] = make_empty_board()
 
 
 @app.get("/board", response_model=Board)
-async def board():
+async def board() -> Board:
     return state["board"]
 
 
 @app.post("/move", response_model=Board)
-async def create_move(move: Move):
+async def create_move(move: Move) -> Board:
     board = state["board"]
     if move.slot_index not in range(len(board.slots)):
         raise HTTPException(status_code=404, detail="Invalid entry")
