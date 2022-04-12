325db007352aad6e7bda4c895b55e281328eda4a
Yuyi <yuyi365@gmail.com>
2022-04-12 11:55:25 -0400

    ci/cd flake8 updates

--- backend/tic_tac_toe_api/tables.py
+++ backend/tic_tac_toe_api/tables.py
@@ -1,21 +1,38 @@
-from sqlalchemy import ARRAY, MetaData, Table, BigInteger, Column, String, DateTime, ForeignKey, Enum
+from sqlalchemy import (
+    MetaData,
+    Table,
+    BigInteger,
+    Column,
+    String,
+    DateTime,
+    ForeignKey,
+    Enum,
+)
 from sqlalchemy.dialects.postgresql import ARRAY
 import datetime
 import enum
 
 metadata = MetaData()
 
+
 class PlayerIx(enum.Enum):
     PLAYER_ONE = 1
     PLAYER_TWO = 2
+
 
 games = Table(
     "games",
     metadata,
     Column("id", BigInteger, primary_key=True, autoincrement=True),
     Column("winning_player_id", BigInteger, nullable=True),
-    Column("created_at", DateTime(timezone=True), default=datetime.datetime.utcnow, nullable=False)
+    Column(
+        "created_at",
+        DateTime(timezone=True),
+        default=datetime.datetime.utcnow,
+        nullable=False,
+    ),
 )
+
 
 boards = Table(
     "boards",
@@ -25,12 +42,19 @@ boards = Table(
     Column("board", ARRAY(Enum(PlayerIx)), nullable=False),
 )
 
+
 players = Table(
     "players",
     metadata,
     Column("id", BigInteger, primary_key=True, autoincrement=True),
-    Column("created_at", DateTime(timezone=True), default=datetime.datetime.utcnow, nullable=False)
+    Column(
+        "created_at",
+        DateTime(timezone=True),
+        default=datetime.datetime.utcnow,
+        nullable=False,
+    ),
 )
+
 
 matches = Table(
     "matches",
@@ -39,5 +63,10 @@ matches = Table(
     Column("token", String, nullable=False),
     Column("game_id", BigInteger, ForeignKey("players.id"), nullable=False),
     Column("player_id", BigInteger, ForeignKey("games.id"), nullable=False),
-    Column("created_at", DateTime(timezone=True), default=datetime.datetime.utcnow, nullable=False)
+    Column(
+        "created_at",
+        DateTime(timezone=True),
+        default=datetime.datetime.utcnow,
+        nullable=False,
+    ),
 )
