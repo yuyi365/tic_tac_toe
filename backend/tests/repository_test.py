01c971c63b29141b66b1193ac814707c09856e84
Yuyi Li <90458697+yuyi365@users.noreply.github.com>
2022-04-20 09:32:19 -0400

    Update backend/tests/repository_test.py
    Co-authored-by: pierce-edmiston-8L <66479218+pierce-edmiston-8L@users.noreply.github.com>

--- backend/tests/repository_test.py
+++ backend/tests/repository_test.py
@@ -5,10 +5,9 @@ from sqlalchemy.sql import select
 
 def test_insert_game_with_valid_pin(db_conn):
     insert_game(db_conn, "4765")
-    query = select(games)
-    result = db_conn.execute(query).first()
-    assert result.pin == "4765"
-    assert result.id == 1
+    results = db_conn.execute(select(games)).fetchall()
+    assert len(results) == 1
+    assert results[0].pin == "4765"
 
 
 def test_insert_game_with_invalid_pin_string_length(db_conn):
