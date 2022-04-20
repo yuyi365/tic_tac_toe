d906597d1ae65aa6eda187d46854eee9e6d696ae
Yuyi Li <90458697+yuyi365@users.noreply.github.com>
2022-04-20 09:33:17 -0400

    Update backend/tic_tac_toe_api/db.py
    Co-authored-by: pierce-edmiston-8L <66479218+pierce-edmiston-8L@users.noreply.github.com>

--- backend/tic_tac_toe_api/db.py
+++ backend/tic_tac_toe_api/db.py
@@ -3,4 +3,4 @@ import sqlalchemy
 
 
 def create_engine() -> sqlalchemy.engine.Engine:
-    return sqlalchemy.create_engine(os.getenv("SQLALCHEMY_CONN"))
+    return sqlalchemy.create_engine(os.getenv("SQLALCHEMY_CONN"), future=True)
