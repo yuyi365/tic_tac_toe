2bb6be8368ac611df388b2b80ecc12506c3e129d
Yuyi Li <90458697+yuyi365@users.noreply.github.com>
2022-04-20 09:37:04 -0400

    Update backend/tests/service_test.py
    Co-authored-by: pierce-edmiston-8L <66479218+pierce-edmiston-8L@users.noreply.github.com>

--- backend/tests/service_test.py
+++ backend/tests/service_test.py
@@ -22,6 +22,7 @@ def test_service_handles_pin_already_exists(mock_make_pin, db_conn):
     second_pin = "1234"
     db_conn.execute(games.insert().values(pin=first_pin))
     mock_make_pin.side_effect = [first_pin, second_pin]
+
     new_game_result = create_new_game(db_conn)
-    assert new_game_result["pin"] != first_pin
+    
     assert new_game_result["pin"] == second_pin
