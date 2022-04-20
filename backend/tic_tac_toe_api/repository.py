1b316f5d94e4cc76478a8f5d3e3b742c4e1e6466
Yuyi <yuyi365@gmail.com>
2022-04-20 09:52:12 -0400

    updated with all MR comments

--- backend/tic_tac_toe_api/repository.py
+++ backend/tic_tac_toe_api/repository.py
@@ -3,4 +3,16 @@ from .tables import games
 
 
 def insert_game(conn: sqlalchemy.engine.Connection, pin: str) -> int:
+<<<<<<< HEAD
     return conn.execute(games.insert().values(pin=pin).returning(games.c.id)).first().id
+=======
+    try:
+        return (
+            conn.execute(games.insert().values(pin=pin).returning(games.c.id))
+            .first()
+            .id
+        )
+    except DataError as e:
+        error = str(e.__dict__["orig"])
+        return error
+>>>>>>> 1c34d96 (updated with all MR comments)
