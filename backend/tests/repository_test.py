4df4199da30c8f8f6cf38f4b42a968663f723b13
Yuyi <yuyi365@gmail.com>
2022-04-21 15:49:24 -0400

    updated tests for the settings endpoint

--- backend/tests/repository_test.py
+++ backend/tests/repository_test.py
@@ -1,6 +1,6 @@
+from tic_tac_toe_api.service import create_new_game
 from tic_tac_toe_api.repository import insert_game, insert_settings
 from tic_tac_toe_api.tables import games, settings
-from tic_tac_toe_api.game import Player, make_default_tokens
 from sqlalchemy.sql import select
 
 
@@ -12,11 +12,10 @@ def test_insert_game_with_valid_pin(db_conn):
 
 
 def test_insert_settings_with_valid_game_id(db_conn):
-    make_default_tokens()
-    insert_game(db_conn, "4765")
-    insert_settings(db_conn, 1, Player.ONE, Player.TWO)
-    results = db_conn.execute(select(settings)).fetchall()
-    assert len(results) == 1
-    assert results[0].game_id == 1
-    assert results[0].player_one_token == Player.ONE
-    assert results[0].player_two_token == Player.TWO
+    new_game = create_new_game(db_conn)
+    results = insert_settings(db_conn, new_game["game_id"], "🦄", "🍄")
+    data = db_conn.execute(select(settings)).fetchall()
+    assert results == 1
+    assert data[0].game_id == new_game["game_id"]
+    assert data[0].player_one_token == "🦄"
+    assert data[0].player_two_token == "🍄"
