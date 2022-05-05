591bc1e32e2f1f92a4b66ace13921935f1446852
Yuyi Li <90458697+yuyi365@users.noreply.github.com>
2022-05-05 14:11:24 -0400

    Update frontend/src/index.tsx
    Co-authored-by: pierce-edmiston-8L <66479218+pierce-edmiston-8L@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,7 @@ import ReactDOM from "react-dom";
 import { AppState } from "./appStates";
 import App from "./components/App";
 
-const [appState, setAppState] = useState<AppState>(AppState.Landing);
 ReactDOM.render(
-  <App appState={appState} setAppState={setAppState} />,
+  <App appState={AppState.Landing} />,
   document.getElementById("root")
 );
